cc5b95080454018cef607515e0440ea6bd7e3b3f
Jeroen Swart <jswart@quintor.local>
2017-03-12 16:53:24 +0100

    Fix build-number environment variable.

--- _BuildScripts/Update-Version.ps1
+++ _BuildScripts/Update-Version.ps1
@@ -1,5 +1,5 @@
 
-$version = $env:TF_BUILD_BUILDNUMBER.Split('_') | Select-Object -Last 1
+$version = $env:BUILD_BUILDNUMBER.Split('_') | Select-Object -Last 1
 $nuspecFilePath = Join-Path -Path $PSScriptRoot -ChildPath '..\nuget\TFSBuildScripts.nuspec'
 
 [xml]$nuspec = Get-Content -Path $nuspecFilePath
