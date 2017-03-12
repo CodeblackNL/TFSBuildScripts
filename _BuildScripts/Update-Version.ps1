b076280eeea60f29fa3c178bea4cf83a31b4f791
Jeroen Swart <jswart@quintor.local>
2017-03-12 16:43:23 +0100

    Add build-script for versioning during TFS Build.

---
+++ _BuildScripts/Update-Version.ps1
@@ -0,0 +1,7 @@
+
+$version = $env:TF_BUILD_BUILDNUMBER.Split('_') | Select-Object -Last 1
+$nuspecFilePath = Join-Path -Path $PSScriptRoot -ChildPath '..\nuget\TFSBuildScripts.nuspec'
+
+[xml]$nuspec = Get-Content -Path $nuspecFilePath
+$nuspec.package.metadata.version = "$version"
+$nuspec.Save($nuspecFilePath)
